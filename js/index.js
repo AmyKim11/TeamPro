e1821c7fd286b21e598ec47b24744aa7cc1074e2
Amykim11 <kcass041@gmail.com>
2022-07-13 16:25:03 +0900

    fix footer arrange and scroll event.1

--- js/index.js
+++ js/index.js
@@ -3,26 +3,20 @@ let clientsImg = document.querySelectorAll(".client_CI");
 let clientImgWrap = document.querySelector("ul"); 
 let plusButton = document.querySelector(".button");  
 // let countVisible = document.querySelectorAll("li[style='display: none']");
-// function b (){
-//    document.write(countVisible);
-// }
-// b();
-// console.log(countVisible);
-// const smallSize = window.matchMedia("(max-width: 940px)").matches;
-// const middleSize = window.matchMedia("(min-width:941px) and (max-width:1217px)");
-// const wideSize = window.matchMedia("(max-width: 1218px)");
-let clientArray = [];
 
-function  clientsImagesClass () {
-   // let d = i+1;
-   // clientsImg.className = classN;
-   // clientImgWrap = parent;
-   // clientsImg[i].style.animation = `0.${i}1s ease-in 0.1s`;
-   // setTimeout(() => {
-   //    clientsImagesClass(clientsImg[i], clientImgWrap);
-   // }, i*1000);
+let clientArray = [];
+//스크롤시 실행되는 지그재그 이벤트
+window.onload = function() {
+   window.addEventListener("scroll", function() {
+      scrollEvent();
+      // e.preventDefault(); 
+   });
+}
+   
+// 해상도 1218px~ 이상일때 지그재그 애니메이션 실행 함수
+function  overWidth1217 () {
       
-      let v = 8;
+      let fixNum = 8;
       for (let i = 0; i < clientsImg.length; i++) {
          
          if (i < 9) {
@@ -30,9 +24,9 @@ function  clientsImagesClass () {
                clientsImg[i].style.opacity = 1;
             }, i * 200)
          } else if (i >= 9 && i < 18) {
-            let j = i + v;
+            let j = i + fixNum;
             let e = 26;
-            v = v - 2;
+            fixNum = fixNum - 2;
             setTimeout(function () {
                clientsImg[j].style.opacity = 1;
                // i=i+1;
@@ -48,19 +42,19 @@ function  clientsImagesClass () {
          }
         
    }
-
+// 해상도 1217px 이하일때 지그재그 애니메이션 실행 함수
    function width1217(){
          
-   let v = 7;
+   let fixNum = 7;
    for (let i = 0; i < clientsImg.length-3; i++) {
       if (i < 8) {
          setTimeout(function () {
             clientsImg[i].style.opacity = 1;
          }, i * 200)
       } else if (i >= 8 && i < 16) {
-         let j = i + v;
+         let j = i + fixNum;
          let e = 23;
-         v = v - 2;
+         fixNum = fixNum - 2;
          setTimeout(function () {
             clientsImg[j].style.opacity = 1;
             // i=i+1;
@@ -78,55 +72,43 @@ function  clientsImagesClass () {
    
    }
 
-   
-
+// 해상도 940px 이하일때 애니메이션 실행 함수
    function width940(){
       for (let i = 0; i < clientsImg.length-15; i++) {
             setTimeout(function () {
                clientsImg[i].style.opacity = 1;
             }, i * 200)
       }
-
-      // if( clientsImg[26].style.opacity === 1 ){
-      //    plusButton.style.display = 'none'; 
-      // }
      
    }
-   // clientsImagesClass();
-   // clearTimeout(clientsImagesClass, width1217, width940);
+
    let startAni = true;
    let $BLOCK = 'block';
-   let NONE =  'none';
-   let sNum1 = [24, 27];
-   let sNum2 = [12, 27];   
+   // let NONE =  'none';
+   // let sNum1 = [24, 27];
+   // let sNum2 = [12, 27];   
+   //해상도에 따라 애니메이션 함수 실행.
    const clientListAni = ()=> {
     
       if(windowWidth > 1218 ){
-         clientsImagesClass();
+         overWidth1217();
          plusButton.style.display = 'none';
-         startAni = false;
-         console.log(windowWidth);
+         // startAni = false;
+         // console.log(windowWidth);
       } else if(windowWidth >= 941 && windowWidth <= 1218){
          width1217();
-         // console.log(clientsImg[26]);
-         // if(clientsImg[26].style.opacity == 1){
-         //    console.log('여기');
-         //    plusButton.style.display = 'none';
-         // } else{
-         //    console.log("here to");
-         //    plusButton.style.display = 'block';
-         // }
-         startAni = false;
-        
-         buttonClickWidth1218(NONE, sNum1[0], sNum1[1]);
+         // startAni = false;
+        console.log("테스트")
+       
+         // buttonClickWidth1218(NONE, sNum1[0], sNum1[1]);
          // width940();
-         console.log(windowWidth);
+         // console.log(windowWidth);
       
       } else if(windowWidth < 941){
          width940();
-         startAni = false;
-         buttonClickWidth1218(NONE, sNum2[0], sNum2[1]);
-         console.log(windowWidth);
+         // startAni = false;
+         // buttonClickWidth1218(NONE, sNum2[0], sNum2[1]);
+         // console.log(windowWidth);
       }
    }
 
@@ -134,8 +116,8 @@ function  clientsImagesClass () {
       clientArray.push(clientsImg[b]);
    }
    
-   clientListAni();
-
+  
+// // 해상도 마다 숨겨진 로고 갯수 및 해당 갯수 로고 안보이게 스타일 줌.
    function buttonClickWidth1218(BLOCK, num1, num2) {
       let sliceArray = [];
       sliceArray = clientArray.slice(num1, num2);
@@ -143,9 +125,10 @@ function  clientsImagesClass () {
       for(let s = 0; s < sliceArray.length;s++){
       sliceArray[s].style.display = BLOCK;
     }
-  
+   //  console.log('나머지로고')
    }
 
+   //플러스 버튼 눌렀을때 나머지 로고 애니메이션 실행.
    function hiddenAni(startNum){
       let e =1;
       for(let v = startNum;v<clientsImg.length;v++){
@@ -158,6 +141,7 @@ function  clientsImagesClass () {
 
    // 플러스 버튼 눌렀을때 나머지 로고 표시
    const clickButtonEvent = () => {
+      console.log('클릭이벤트');
       buttonClickWidth1218($BLOCK);
       if(windowWidth >= 941 && windowWidth <= 1218){
          hiddenAni(24);
@@ -165,8 +149,6 @@ function  clientsImagesClass () {
          hiddenAni(12);
       }
  
-      // plusButton.classList.add('noshow');
-      // console.log(clientsImg[26].style.display);
       if(clientsImg[26].style.display == 'block'){
          console.log('여기');
          plusButton.style.display = 'none';
@@ -175,13 +157,36 @@ function  clientsImagesClass () {
          plusButton.style.display = 'block';
       }
    }
+ // clientListAni();
 
+// 스크롤시 실행되는 함수
+ let scrollEvent = function() {
+      
+   // 사용자 모니터 화면 높이 + 스크롤이 움직인 높이
+   const scrollY = window.scrollY;
+   const innerHeight = window.innerHeight;
+   const scroll = (innerHeight * 0.9) + scrollY;
+   
+   Array.prototype.forEach.call(clientsImg, function(li) {
+      // console.log(li.offsetTop);
+      // 객체 위치와 높이 비교 : 화면에 표출되는 높이인지 체크
+      if(li.offsetTop < scroll) {
+         clientListAni();
+         // console.log('스크롤');
+         // window.clearTimeout(clientListAni);
+       } 
+       plusButton.addEventListener("click", clickButtonEvent);
+  });
+  
+
+}
+
+// 해당 고객 기업 리스트 파트에서 새로고침시 실행되게 함수 한번 더 적어줌.
+scrollEvent();
 
    // console.log(buttonClickWidth1218);
-   plusButton.addEventListener("click", clickButtonEvent);
-   // window.addEventListener("resize", clientListAni);
-   // plusButton.removeEventListener("click", clickButtonEvent);
-   // window.addEventListener("resize", resizeMiddle);
 
-   window.clearTimeout(clientListAni);
+   // window.addEventListener("scroll", scrollEvent);
+ 
+
    
